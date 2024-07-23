f80857172287308f41a982681ac9cc2066d43996
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-07-23 10:53:27 -0300

    Configuring theme

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+// src/theme.ts
+export const theme = {
+  colors: {
+    background: "#fff",
+    primary: "#36231C",
+    secondaryBackground: "#F8F9FA",
+    accent: "#...",
+  },
+  fonts: {
+    // ... your font families and sizes
+  },
+};
+
+// Optional: create a global style using `createGlobalStyle` from styled-components
